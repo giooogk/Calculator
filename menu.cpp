0bb181686ba17f81c8f82d53277a87dda06d57bb
Giovanna Karla de Macedo Felix <Giovanna@localhost.localdomain>
2019-04-20 18:50:35 -0300

    prontinho

--- menu.cpp
+++ menu.cpp
@@ -10,31 +10,34 @@ int main(){
   int a;
   int b;
   int op = 0;
+  
   cout <<"Digite o 1o valor: ";
   cin >> a;
-  cin.ignore ();
+  
   cout <<"Digite o 2o valor: ";
   cin >> b;
-  cin.ignore ();
   
-  cout <<"Que operacao deseja realizar?\n 1. Adicao  2.Subtracao 3.Multiplicacao 4.Divisao\n";
-  cin.ignore ();
+  cout <<"Que operacao deseja realizar?" << endl << "1. Adicao  2.Subtracao 3.Multiplicacao 4.Divisao" << endl;
+  cin >> op;
   
   switch(op){
+    
     case 1:
-      int soma(int a, int b);
+      cout << soma(a,b);
       break;
+    
     case 2:
-      int sub(int a, int b);
+      cout << sub(a,b);
       break;
+    
     case 3:
-      int mut(int a, int b);
+      cout << mut(a,b);
       break;
+    
     case 4:
-      int divi(int a, int b);
+      cout << divi(a,b);
       break;
       
-  }
-  system ("pause");
+  };
   return 0;
 }
